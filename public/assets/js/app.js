77b444d390f03ed38b3a390cc0fa458f200ba3fd
reyesr3 <reyesr3@gmail.com>
2016-12-16 22:33:56 -0800

    Added Portfolio page with initial animation

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,37 @@
+function collapseNavbar() {
+    $(".navbar-nav li a" ).on( "click", function() {
+        if(!$(".navbar-default").hasClass("top-nav-collapse")) {
+          $(".navbar-default").addClass("top-nav-collapse");
+        };
+    });
+}
+
+function currentTab() {
+  $(".navbar-nav li" ).on( "click", function() {
+    $(".navbar-nav li").removeClass("current-tab");
+    $(".navbar-nav li a").removeClass("current-font");         
+      $(this).addClass("current-tab");
+      $(this).children().addClass("current-font");
+  });
+}
+
+$(document).ready(function(){
+  collapseNavbar();
+  currentTab();
+
+  $("#portfolio-nav").click(function(e){
+    e.preventDefault();
+    $.ajax({url: "/views/portfolio.html", success: function(result){
+      console.log(result);
+        $(".main-content").addClass('move-down');
+        $(".main-content").html(result);
+    }});
+  });
+
+});
+
 var $item = $('.carousel .item'); 
+var $portfolio = $('#portfolio-content')
 var $wHeight = $(window).height();
 $item.eq(0).addClass('active');
 $item.height($wHeight); 
@@ -23,3 +56,9 @@ $('.carousel').carousel({
   interval: 6000,
   pause: "false"
 });
+
+// $('ul.nav li.dropdown').hover(function() {
+//   $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+// }, function() {
+//   $(this).find('.dropdown-menu').stop(true, true).fadeOut();
+// });
--- server.js
+++ server.js
@@ -23,8 +23,8 @@ router.get("/contact",function(req,res){
 
 
 // prepare server
+app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/public'));
-
 // app.use('/', express.static(__dirname + '/www')); // redirect root
 // app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 // app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
